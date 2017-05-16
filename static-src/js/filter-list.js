42e9af7214c3594304ccd0b136609c533dcc553b
Karissa Demi <k@karissademi.com>
2017-05-16 11:21:22 -0400

    Filter by title as well

--- static-src/js/filter-list.js
+++ static-src/js/filter-list.js
@@ -2,7 +2,7 @@ var List = require('list.js');
 
 module.exports = $(function(){
   var options = {
-    valueNames: [ 'content' ]
+    valueNames: [ 'title', 'content' ]
   };
 
   new List('filter-list', options);
