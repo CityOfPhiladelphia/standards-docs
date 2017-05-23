66b90e9910ba5d624ce9c8301b2d545bc90889c5
Karissa Demi <k@karissademi.com>
2017-05-23 12:46:17 -0400

    Add anchor offsets

--- static-src/js/main.js
+++ static-src/js/main.js
@@ -1,1 +1,15 @@
 require('./filter-list.js');
+
+function anchorOffset() {
+  const $anchor = $(':target');
+  const fixedElementHeight = $('.sticky.is-stuck').outerHeight();
+  if ($anchor.length > 0)
+    window.scrollTo(0, $anchor.offset().top - fixedElementHeight);
+}
+$(window).on('hashchange load', anchorOffset);
+$('body').on('click', "a[href^='#']", function (ev) {
+  if (window.location.hash === $(this).attr('href')) {
+    ev.preventDefault();
+    anchorOffset();
+  }
+});
