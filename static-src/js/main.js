e30603584678af4d102c49296399c7020f1dbca5
Karissa Demi <k@karissademi.com>
2017-05-23 12:50:58 -0400

    Fire on page load

--- static-src/js/main.js
+++ static-src/js/main.js
@@ -2,10 +2,11 @@ require('./filter-list.js');
 
 function anchorOffset() {
   const $anchor = $(':target');
-  const fixedElementHeight = $('.sticky.is-stuck').outerHeight();
+  const fixedElementHeight = $('.sticky').outerHeight();
   if ($anchor.length > 0)
     window.scrollTo(0, $anchor.offset().top - fixedElementHeight);
 }
+
 $(window).on('hashchange load', anchorOffset);
 $('body').on('click', "a[href^='#']", function (ev) {
   if (window.location.hash === $(this).attr('href')) {
@@ -13,3 +14,6 @@ $('body').on('click', "a[href^='#']", function (ev) {
     anchorOffset();
   }
 });
+if (window.location.hash) {
+  $(window).trigger('hashchange');
+}
